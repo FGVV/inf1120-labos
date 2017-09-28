729127a191534c5f75d8c94d5320abfa114b84b7
Vincent <vi.lirette@gmail.com>
2017-09-28 16:43:28 -0400

    Delete doc folder.

--- Labo2/Labo2_2.java
+++ Labo2/Labo2_2.java
@@ -1,13 +1,13 @@
 class Labo2_2 {
 
     public static void main ( String [] params ) {
-    
+
       int nbSecondesInit; // nombre de secondes initial
       int nbSecondes;      // nombre de secondes apres calculs
       int nbMinutes;       // nombre de minutes apres calculs
       int nbHeures;        // nombre de heures apres calculs
       int nbJours;         // nombre de jours apres calculs
-      
+
       nbSecondesInit = 0;
       nbMinutes = nbSecondesInit / 60;
       nbSecondes = nbSecondesInit % 60;
@@ -15,12 +15,12 @@ class Labo2_2 {
       nbMinutes = nbMinutes % 60;
       nbJours = nbHeures / 24;
       nbHeures = nbHeures % 24;
-      
+
       System.out.println ( nbSecondesInit + " secondes = " + nbJours + " jour(s) + "
                                                         + nbHeures + " heure(s) + "
                                                         + nbMinutes + " minute(s) + "
                                                         + nbSecondes + " seconde(s)." );
-                                                        
+
       nbSecondesInit = 23;
       nbMinutes = nbSecondesInit / 60;
       nbSecondes = nbSecondesInit % 60;
@@ -28,7 +28,7 @@ class Labo2_2 {
       nbMinutes = nbMinutes % 60;
       nbJours = nbHeures / 24;
       nbHeures = nbHeures % 24;
-      
+
       System.out.println ( nbSecondesInit + " secondes = " + nbJours + " jour(s) + "
                                                         + nbHeures + " heure(s) + "
                                                         + nbMinutes + " minute(s) + "
@@ -41,12 +41,12 @@ class Labo2_2 {
       nbMinutes = nbMinutes % 60;
       nbJours = nbHeures / 24;
       nbHeures = nbHeures % 24;
-      
+
       System.out.println ( nbSecondes + " secondes = " + nbJours + " jour(s) + "
                                                         + nbHeures + " heure(s) + "
                                                         + nbMinutes + " minute(s) + "
                                                         + nbSecondes + " seconde(s)." );
-                                                        
+
       nbSecondesInit = 11842;
       nbMinutes = nbSecondesInit / 60;
       nbSecondes = nbSecondesInit % 60;
@@ -54,12 +54,12 @@ class Labo2_2 {
       nbMinutes = nbMinutes % 60;
       nbJours = nbHeures / 24;
       nbHeures = nbHeures % 24;
-      
+
       System.out.println ( nbSecondesInit + " secondes = " + nbJours + " jour(s) + "
                                                         + nbHeures + " heure(s) + "
                                                         + nbMinutes + " minute(s) + "
                                                         + nbSecondes + " seconde(s)." );
-                                                        
+
       nbSecondesInit = 90777;
       nbMinutes = nbSecondesInit / 60;
       nbSecondes = nbSecondesInit % 60;
@@ -67,12 +67,12 @@ class Labo2_2 {
       nbMinutes = nbMinutes % 60;
       nbJours = nbHeures / 24;
       nbHeures = nbHeures % 24;
-      
+
       System.out.println ( nbSecondesInit + " secondes = " + nbJours + " jour(s) + "
                                                         + nbHeures + " heure(s) + "
                                                         + nbMinutes + " minute(s) + "
                                                         + nbSecondes + " seconde(s)." );
-                                                        
+
     } // main
-    
+
 } // Labo2_2
